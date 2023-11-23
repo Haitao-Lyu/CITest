3ad88143b402b0cce86467f573406e08b7154f42
Haitao-Lyu <hlyu@sumo-digital.com>
2023-11-23 14:17:31 +0000

    Add TestCases

--- OilTycoonOceanOdyssey/playbuffer/UnitTest/UnitTest.cpp
+++ OilTycoonOceanOdyssey/playbuffer/UnitTest/UnitTest.cpp
@@ -1,7 +1,13 @@
 #define CATCH_CONFIG_MAIN
 #include "UnitTest.h"
+#include "GameTool/Math2D.h"
 
 TEST_CASE("Sanity checks :)", "[Sanity]") {
 	REQUIRE(1 == 1);
 	REQUIRE(2 != 1);
+}
+
+TEST_CASE("Math2D checks :)", "[Sanity]") 
+{
+
 }
